a05929c3705368c68c30cbdb3b3fd93632eb51cc
Daryl Rowland <daryl@cloudyclear.com>
2014-01-21 18:10:35 +0000

    Commited more pages for the site

---
+++ js/app/controllers/main-controller.js
@@ -0,0 +1,14 @@
+app.controller('MainController', ['$scope', '$http',
+    function MainController($scope, $http) {
+
+        $scope.example1Data = [
+            {title: "Norwich"},
+            {title: "London"},
+            {title: "San Francisco"}
+
+
+
+        ]
+
+    }
+]);
